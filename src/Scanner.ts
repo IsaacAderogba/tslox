a88bf7784e2ff976abb38654e48d331cdc4f06ac
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-10-18 20:28:05 +0100

    Add core of scanToken

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -1,13 +1,77 @@
 import { LoxApi } from "./LoxApi";
 import { Token } from "./Token";
+import { TokenType } from "./TokenType";
 
 export class Scanner {
-  lox: LoxApi;
-  source: string;
-  tokens: Token[] = [];
+  private lox: LoxApi;
+  private source: string;
+  private start = 0;
+  private current = 0;
+  private line = 1;
+  private tokens: Token[] = [];
 
   constructor(lox: LoxApi, source: string) {
     this.lox = lox;
     this.source = source;
+  }
+
+  scanTokens(): Token[] {
+    while (!this.isAtEnd()) {
+      this.start = this.current;
+      this.scanToken();
+    }
+
+    this.tokens.push(new Token("EOF", "", null, this.line));
+    return this.tokens;
+  }
+
+  private scanToken(): void {
+    const c = this.advance();
+
+    switch (c) {
+      case "(":
+        this.addToken("LEFT_PAREN");
+        break;
+      case ")":
+        this.addToken("RIGHT_PAREN");
+        break;
+      case "{":
+        this.addToken("LEFT_BRACE");
+        break;
+      case "}":
+        this.addToken("RIGHT_BRACE");
+        break;
+      case ",":
+        this.addToken("COMMA");
+        break;
+      case ".":
+        this.addToken("DOT");
+        break;
+      case "-":
+        this.addToken("MINUS");
+        break;
+      case "+":
+        this.addToken("PLUS");
+        break;
+      case ";":
+        this.addToken("SEMICOLON");
+        break;
+      case "*":
+        this.addToken("STAR");
+        break;
+    }
+  }
+
+  private isAtEnd(): boolean {
+    return this.current >= this.source.length;
+  }
+
+  private advance(): string {
+    return this.source.charAt(this.current++);
+  }
+
+  private addToken(type: TokenType, literal: object | null = null): void {
+    const text = this.source.substring(this.start, this.current);
+    this.tokens.push(new Token(type, text, literal, this.line));
   }
 }
--- src/Token.ts
+++ src/Token.ts
@@ -3,10 +3,15 @@ import { TokenType } from "./TokenType";
 export class Token {
   type: TokenType;
   lexeme: string;
-  literal: object;
+  literal: object | null;
   line: number;
 
-  constructor(type: TokenType, lexeme: string, literal: object, line: number) {
+  constructor(
+    type: TokenType,
+    lexeme: string,
+    literal: object | null,
+    line: number
+  ) {
     this.type = type;
     this.lexeme = lexeme;
     this.literal = literal;
