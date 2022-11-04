64170cf6265411b4b43aeb7e29d7e74a0201a261
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-11-04 19:21:19 +0000

    Start fleshing out Parser

---
+++ src/Parser.ts
@@ -0,0 +1,123 @@
+import { BinaryExpr, Expr, GroupingExpr, LiteralExpr, UnaryExpr } from "./Expr";
+import { Token } from "./Token";
+import { TokenType } from "./TokenType";
+
+export class Parser {
+  private tokens: Token[] = [];
+  private current = 0;
+
+  constructor(tokens: Token[]) {
+    this.tokens = tokens;
+  }
+
+  private expression(): Expr {
+    return this.equality();
+  }
+
+  private equality(): Expr {
+    let expr = this.comparison();
+
+    while (this.match("BANG_EQUAL", "EQUAL_EQUAL")) {
+      const operator = this.previous();
+      const right = this.comparison();
+      expr = new BinaryExpr(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private comparison(): Expr {
+    let expr = this.term();
+
+    while (this.match("GREATER", "GREATER_EQUAL", "LESS", "LESS_EQUAL")) {
+      const operator = this.previous();
+      const right = this.term();
+      expr = new BinaryExpr(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private term(): Expr {
+    let expr = this.factor();
+
+    while (this.match("MINUS", "PLUS")) {
+      const operator = this.previous();
+      const right = this.factor();
+      expr = new BinaryExpr(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private factor(): Expr {
+    let expr = this.unary();
+
+    while (this.match("SLASH", "STAR")) {
+      const operator = this.previous();
+      const right = this.unary();
+      expr = new BinaryExpr(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private unary(): Expr {
+    if (this.match("BANG", "MINUS")) {
+      const operator = this.previous();
+      const right = this.unary();
+      return new UnaryExpr(operator, right);
+    }
+
+    return this.primary();
+  }
+
+  private primary(): Expr {
+    if (this.match("FALSE")) return new LiteralExpr(false);
+    if (this.match("TRUE")) return new LiteralExpr(true);
+    if (this.match("NIL")) return new LiteralExpr(null);
+
+    if (this.match("NUMBER", "STRING")) {
+      return new LiteralExpr(this.previous().literal);
+    }
+
+    if (this.match("LEFT_PAREN")) {
+      const expr = this.expression();
+      this.consume("RIGHT_PARENT", "Expect ')' after expression.");
+      return new GroupingExpr(expr);
+    }
+  }
+
+  private match(...types: TokenType[]): boolean {
+    for (const type of types) {
+      if (this.check(type)) {
+        this.advance();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private check(type: TokenType): boolean {
+    if (this.isAtEnd()) return false;
+    return this.peek().type === type;
+  }
+
+  private advance(): Token {
+    if (!this.isAtEnd()) this.current++;
+    return this.previous();
+  }
+
+  private isAtEnd(): boolean {
+    return this.peek().type === "EOF";
+  }
+
+  private peek(): Token {
+    return this.tokens[this.current];
+  }
+
+  private previous(): Token {
+    return this.tokens[this.current - 1];
+  }
+}
